499051ffcd4ac1bb20c88a3c6a2986b53ad31c91
Joel Jossie <joeljossie@gmail.com>
2023-07-17 16:14:15 -0600

    fix issues with loans and relations showing up in BorrowerInventory

--- mobile-app/src/components/screens/BorrowerInventory.tsx
+++ mobile-app/src/components/screens/BorrowerInventory.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Button, Center, Column, ScrollView, Text} from 'native-base';
-import LenderInventoryItem from "../LenderInventoryItem";
+import React, {useEffect} from 'react';
+import {Button, Column, ScrollView, Text} from 'native-base';
 import Spacer from "../utilities/Spacer";
 import {useNavigation} from "@react-navigation/native";
 import {useMyTools} from "../../utils/hooks/useMyTools";
+import LoanContextItem from "../LoanContextItem";
+import {getRelationById, getRelationId} from "../../controllers/relation";
+import {ILoan, IRelation} from "../../models/Relation";
 
 export interface BorrowerInventoryProps {
 
@@ -12,25 +14,53 @@ export interface BorrowerInventoryProps {
 const BorrowerInventory: React.FC<BorrowerInventoryProps> = ({}) => {
 
   const navigation = useNavigation();
-  const {borrowingToolsList} = useMyTools();
+  const {borrowingLoansList} = useMyTools();
+
+  const [loansAndRelations, setLoansAndRelations] = React.useState<{loan: ILoan, relation: IRelation }[]>([]);
+
+  // console.log("❇️BorrowingLoansList:", JSON.stringify(borrowingLoansList, null, 2));
+
+
+  const getRelationFromLoan = async (loan: ILoan) => {
+    const relationID = getRelationId(loan.borrowerUid, loan.lenderUid);
+    console.log(`❇️getting relation ${relationID}`);
+    const relation = await getRelationById(relationID);
+    console.log(`❇️relation ${relationID} : `, JSON.stringify(relation, null, 2));
+    return {relation, loan}
+  };
+
+
+  useEffect(() => {
+    console.log("❇️BorrowerInventory useEffect()");
+    let promises: Promise<{loan: ILoan, relation: IRelation }>[] = [];
+
+    borrowingLoansList.forEach(loan => promises.push(getRelationFromLoan(loan)));
+
+    Promise.all(promises).then((results) => {
+      console.log("❇️BorrowerInventory loansAndRelations results:", JSON.stringify(results, null, 2));
+      setLoansAndRelations(results);
+    });
+  }, [borrowingLoansList]);
+
+
 
   return (
       <ScrollView>
-        <Column p={4}>
-          <Text p={4} bold fontSize="4xl">My Tools</Text>
-          {(borrowingToolsList.length > 0)
-              ? borrowingToolsList.map(tool => {
-                return <LenderInventoryItem navigation={navigation}
-                                            key={tool.id + tool.name}
-                                            tool={tool}/>;
+        <Column p={4} flex={1} h={"100%"}>
+          <Text p={4} bold fontSize="4xl">Borrowed Tools</Text>
+          {(loansAndRelations && loansAndRelations.length > 0)
+              ?
+              loansAndRelations.map( ({loan, relation}) => {
+                return (<LoanContextItem key={loan.id} relation={relation} loan={loan}/>);
               })
 
-              : <Center>
+              :
+              <Column justifyContent={"space-around"} h={"100%"} alignItems={"center"} space={4} flex={1} >
                 <Text>You aren't borrowing any tools right now. Go find some!</Text>
-                <Button onPress={() => {
+                <Button variant={"solid"} size={"lg"} onPress={() => {
                   navigation.getParent()!.navigate("SearchBrowse");
                 }}>Browse Tools</Button>
-              </Center>}
+              </Column>}
         </Column>
         <Spacer/>
       </ScrollView>
--- mobile-app/src/utils/hooks/useMyTools.ts
+++ mobile-app/src/utils/hooks/useMyTools.ts
@@ -1,15 +1,16 @@
 import React from 'react';
-import {collection, onSnapshot, query, where} from "firebase/firestore";
+import {collection, collectionGroup, onSnapshot, query, where} from "firebase/firestore";
 import {db} from "../../config/firebase";
 import {ITool} from "../../models/Tool";
 import {useAuthentication} from "./useAuthentication";
+import {ILoan} from "../../models/Relation";
 
-export function useMyTools(): { lendingToolsList: ITool[], borrowingToolsList: ITool[] } {
+export function useMyTools(): { lendingToolsList: ITool[], borrowingLoansList: ILoan[] } {
 
   const [lendingToolsList, setLendingToolsList] = React.useState<ITool[]>([]);
-  const [borrowingToolsList, setBorrowingToolsList] = React.useState<ITool[]>([]);
+  const [borrowingLoansList, setBorrowingLoansList] = React.useState<ILoan[]>([]);
 
-  const {authUser} = useAuthentication();
+  const {authUser, user} = useAuthentication();
 
   console.log("🛠useMyTools()");
 
@@ -21,10 +22,10 @@ export function useMyTools(): { lendingToolsList: ITool[], borrowingToolsList: I
         where("lenderUid", "==", authUser.uid),
     );
 
-    const borrowingQuery = query(collection(db, "tools"),
-        where("lenderUid", "!=", authUser.uid),
-        where("holderUid", "==", authUser.uid),
-    ); // Do we want to add a query for tools we're interested in but not holding just yet?
+    // const borrowingQuery = query(collection(db, "tools"),
+    //     where("lenderUid", "!=", authUser.uid),
+    //     where("holderUid", "==", authUser.uid),
+    // ); // Do we want to add a query for tools we're interested in but not holding just yet?
 
     const lendingUnsubscribe = onSnapshot(lendingQuery, (snapshot) => {
       const docDataList: ITool[] = [];
@@ -34,13 +35,26 @@ export function useMyTools(): { lendingToolsList: ITool[], borrowingToolsList: I
       setLendingToolsList(docDataList);
     });
 
-    const borrowingUnsubscribe = onSnapshot(borrowingQuery, (snapshot) => {
-      const docDataList: ITool[] = [];
-      snapshot.forEach(document => {
-        docDataList.push({id: document.id, ...document.data()} as ITool);
+    let borrowingUnsubscribe = () => {
+    };
+
+    if (user?.relations) {
+      console.log("🛠️Constructing borrowerQuery");
+      const borrowingQuery = query(
+          collectionGroup(db, "loans"),
+          where("borrowerUid", "==", authUser.uid),
+      );
+
+      borrowingUnsubscribe = onSnapshot(borrowingQuery, (snapshot) => {
+        console.log("🛠️borrowingQuery results:", snapshot.size);
+        const docDataList: ILoan[] = [];
+        snapshot.forEach(document => {
+          // console.log("🛠️Found borrowing loan:", JSON.stringify(document.data(), null, 2));
+          docDataList.push({id: document.id, ...document.data()} as ILoan);
+        });
+        setBorrowingLoansList(docDataList);
       });
-      setBorrowingToolsList(docDataList);
-    });
+    }
 
     return () => {
       lendingUnsubscribe();
@@ -48,5 +62,5 @@ export function useMyTools(): { lendingToolsList: ITool[], borrowingToolsList: I
     };
   }, [authUser]);
 
-  return {lendingToolsList, borrowingToolsList};
+  return {lendingToolsList, borrowingLoansList};
 }
