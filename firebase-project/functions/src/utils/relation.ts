2c025ed7491011223f5f5aa94274b2e831e1a6c0
Joel Jossie <joeljossie@gmail.com>
2025-07-01 07:25:29 -0400

    elevate mobile-app folder

--- firebase-project/functions/src/utils/relation.ts
+++
@@ -1,7 +0,0 @@
-
-export function getRelationId(currentUserId: string, otherUserId: string) {
-    // Sort the two user IDs alphabetically to ensure that the relation is unique.
-    const sortedUserIds = [currentUserId, otherUserId].sort();
-    return `${sortedUserIds[0]}-${sortedUserIds[1]}`;
-  }
-  
