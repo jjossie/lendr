381130a62a24abae98e5b69e71f8e4a6403479cf
Joel Jossie <joeljossie@gmail.com>
2025-07-06 21:54:17 -0400

    random linter fix

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,6 +1,6 @@
 export class LendrBaseError extends Error {
   constructor(message?: string) {
-    super("⚠️🛠Lendr Error 🛠⚠️ " + message ?? "");
+    super("⚠️🛠Lendr Error 🛠⚠️ " + message);
   }
 }
 
